b69e1e6fe6b31585cc7c23fe71dc0f2a863001ed
Upadhyaya, Shridhar <shridhar.upadhyaya@mckesson.com>
2020-04-19 18:37:17 -0500

    formatting

--- maven-unit-test/src/main/java/com/mkyong/examples/MessageBuilder.java
+++ maven-unit-test/src/main/java/com/mkyong/examples/MessageBuilder.java
@@ -3,7 +3,11 @@ package com.mkyong.examples;
 public class MessageBuilder {
 
     public static String getHelloWorld(){
+    	
         return "hello shridhar's world now";
+        
+        
+        
     }
 
     public static int getNumber10(){
